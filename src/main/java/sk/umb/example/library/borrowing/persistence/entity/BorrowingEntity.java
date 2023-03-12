d21e385350c89a229cc41d060e267be913a3e419
VOJAKIS <valastan1@gmail.com>
2023-03-12 16:31:27 +0100

    Added attributes to BorrowingEntity

--- src/main/java/sk/umb/example/library/borrowing/persistence/entity/BorrowingEntity.java
+++ src/main/java/sk/umb/example/library/borrowing/persistence/entity/BorrowingEntity.java
@@ -1,5 +1,50 @@
 package sk.umb.example.library.borrowing.persistence.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import sk.umb.example.library.book.persistence.entity.BookEntity;
+import sk.umb.example.library.customer.persistence.entity.CustomerEntity;
+
+@Entity(name = "borrowing")
 public class BorrowingEntity {
+	@Id
+	@GeneratedValue
+	@Column(name = "id_borrowing", unique = true)
+	private Long id;
+
+	@ManyToOne
+	private CustomerEntity customer;
+
+	@ManyToOne
+	private BookEntity book;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public CustomerEntity getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(CustomerEntity customer) {
+		this.customer = customer;
+	}
+
+	public BookEntity getBook() {
+		return book;
+	}
+
+	public void setBook(BookEntity book) {
+		this.book = book;
+	}
+
 	
+
 }
