5127ed0953933e0c2afd63ed2a95d9895e68e2bb
V0ltaroo <nendorans@gmail.com>
2023-03-12 15:29:48 +0100

    [fix]: javax -> jakarta fix

--- src/main/java/sk/umb/example/library/book/persistence/entity/BookEntity.java
+++ src/main/java/sk/umb/example/library/book/persistence/entity/BookEntity.java
@@ -2,10 +2,11 @@ package sk.umb.example.library.book.persistence.entity;
 
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
 
 @Entity(name = "book")
 public class BookEntity {
